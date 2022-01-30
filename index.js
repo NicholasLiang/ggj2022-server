7a69eeb6a9c20e18e753962b6562087b2534c0ba
Nicholas Liang <ssss021418@gmail.com>
2022-01-30 05:23:35 -0800

    trying to deploy to heroku

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const WebSocketServer = require('ws').WebSocketServer;
+'use strict';
 
-const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
 
 var client_n = 0;
 var rooms = [];
